3a02b52de3f8694cabb71a62a6b02cc0f819846c
Jean Fingolo Dias <zinho_dias@hotmail.com>
2015-12-24 15:10:01 -0200

    bootstrap layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def error_tag(model, attribute)
+    if model.errors.has_key? attribute
+      content_tag(
+        :div,
+        model.errors[attribute].first,
+        class: 'help-block help-block-error'
+      )
+    end
+  end
 end
