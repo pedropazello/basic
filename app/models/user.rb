117e72fa395a0374f2942e39339fdcf3d4f30e39
Jean Fingolo Dias <zinho_dias@hotmail.com>
2016-01-07 14:17:42 +0000

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
 	validates_presence_of :first_name, :last_name, :email
+	validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: :invalid_format }
+	
 	has_secure_password
 end
