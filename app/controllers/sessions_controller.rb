c03baf66f0b114da3d686ac5271020744f2635f0
Jean Dias <jeanpeixotodias@gmail.com>
2016-01-11 16:19:11 +0000

    fixed layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.find_by_email(params[:session][:email])
 
@@ -12,12 +12,13 @@ class SessionsController < ApplicationController
         session[:user_id] = @user.id
         format.html { redirect_to '/' }
       else
+        flash[:danger] = "Invalid login or password."
         format.html { render :new }
         #redirect_to '/login'
       end
     end
   end
-  
+
   def destroy
     session[:user_id] = nil
     redirect_to '/'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :users
   root 'users#index'
-  
-  get '/login' => 'sessions#new'
+
+  get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
-  
+
   delete 'logout' => 'sessions#destroy'
 end
