e2d637f04f57db3c36d5ba9b24bc6edf1751a040
Jean Fingolo Dias <zinho_dias@hotmail.com>
2016-07-15 22:33:08 -0300

    add change password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,12 +56,33 @@ class UsersController < ApplicationController
     end
   end
 
+  def change_password
+    @user = current_user
+  end
+
+  #
+  #
+  def update_password
+    @user = User.find(current_user.id)
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user }
+        flash[:success] = t('flash.sucess.update_password')
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to users_url }
+      flash[:success] = t('flash.sucess.delete_user')
       format.json { head :no_content }
     end
   end
@@ -74,7 +95,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :users
   root 'users#index'
 
+  get 'user/change_password' => 'users#change_password', :as => 'change_user_password'
+  put 'user/update_password' => 'users#update_password', :as => 'update_user_password'
+
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
 
